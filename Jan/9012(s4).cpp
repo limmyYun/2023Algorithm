3fd1703b4618a3221c5842d04e6af58722215e2d
HyeLimYun <yhly970@gmail.com>
2023-01-23 23:30:28 +0900

    1월 23일 문제 풀이

--- Jan/9012(s4).cpp
+++ Jan/9012(s4).cpp
@@ -2,10 +2,30 @@
 #include <stdlib.h>
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-int main() {
+int T;
+queue <int> que;
 
+int main() {
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		string str;
+		bool isVPS=false;
+		cin >> str;
+		que = {};
+		for (int j = 0; j < str.length(); j++) {
+			if (str[j] == '(') que.push(1);
+			else if (str[j] == ')' && !que.empty()) que.pop();
+			else if (str[j] == ')' && que.empty()) {
+				isVPS = true;
+				break;
+			}
+		}
+		if (isVPS || que.size() != 0) cout << "NO" << endl;
+		else cout << "YES" << endl;
+	}
 
 }
